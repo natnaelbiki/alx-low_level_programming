5fc5eb296d9e143da44ab9f0d492853a83aa6e9a
natnaelbiki <natnaelbiki@gmail.com>
2022-12-08 05:37:15 -0800

    done with main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 
+
+/**
+ *
+ * *main - my main function
+ *
+ * *
+ *
+ * *
+ *
+ * *Return: return 0
+ *
+ * */
+
+
 int main(void){
-	char text[50];
+	
+	char text[60];
 	strcpy(text, "\"Programming is like building a multilingual puzzle");
 	puts(text);
 	return (0);
