88dcdd1d176e52fc7103cde48a3ff3183b6666ad
natnaelbiki <natnaelbiki@gmail.com>
2022-12-08 06:16:24 -0800

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+
 /**
- *
  *  * main - Entry point
- *
- *   *
- *
+ * 
  *    * Return: Always 0 (Success)
- *
- *     */
+ */
+
 int main(void)
 {
 	char text[60];
