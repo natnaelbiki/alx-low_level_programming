804728123d0b10c4cdfdbedb5985687850d1d749
natnaelbiki <natnaelbiki@gmail.com>
2022-12-13 06:50:47 -0800

    2 alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * function that prints alphabet
+ * Returns: no return
+ */
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
