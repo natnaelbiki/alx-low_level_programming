46560f90dbb0facc9607754d52876a482c8ee0e8
natnaelbiki <natnaelbiki@gmail.com>
2022-12-13 07:53:11 -0800

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign: program to print number sign
+ * Return: returns 0 if 0 1 if positive and -1 if negative
+ * @n - number to be checked
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		return (5);
+}
