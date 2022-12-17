a7a570ca200dd39397a7c89e24fcc0429c3cd9e8
natnaelbiki <natnaelbiki@gmail.com>
2022-12-17 05:14:14 -0800

    trin

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 /**
  * main - cause infinite loop
+ *
  * Description: Program to cause  infinte loop
+ *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 	/**
-	 * i = 0;
+	* i = 0;
 	while (i < 10)
-	{	
+	{
 		putchar(i);
 	}
 	*/
