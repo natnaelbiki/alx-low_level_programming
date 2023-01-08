33b3c3ca9d1da9623e273888bfaba2b41978cf8b
natnaelbiki <natnaelbiki@gmail.com>
2023-01-08 04:15:05 -0800

    finally

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: input pointer
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
