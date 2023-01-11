5e367f1f7418feca82db4d28a1384202da31d2f6
natnaelbiki <natnaelbiki@gmail.com>
2023-01-10 23:45:08 -0800

    av1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,46 +1,5 @@
 #include "main.h"
 /**
- * strtow - splits a string to words
- * @str: string of words to be split
- * Return: double pointer to string
- */
-char **strtow(char *str)
-{
-	char **ptr;
-	int i, k, len, start, end, j = 0;
-	int words = countWords(str);
-
-	if (!str || !countWords(str))
-		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < words; i++)
-	{
-		start = startIndex(str, j);
-		end = endIndex(str, start);
-		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
-		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-				i--;
-			}
-			free(ptr);
-			return (NULL);
-		}
-		for (k = 0; k < len; k++)
-			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
-		j = end + 1;
-	}
-	ptr[i] = NULL;
-	return (ptr);
-}
-/**
  * isSpace - check if char is space or not
  * @c: input char
  * Return: 1 if true else 0
@@ -96,3 +55,49 @@ int countWords(char *s)
 	}
 	return (words);
 }
+/**                                                                      
+ *
+ *  * strtow - splits a string to words                                     
+ *
+ *   * @str: string of words to be split                                     
+ *
+ *    * Return: double pointer to string                                      
+ *
+ *     */                                                                      
+
+char **strtow(char *str)                                                 
+{
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words = countWords(str);
+	
+	if (!str || !countWords(str))
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
+	{
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+		if (!ptr[i])
+		{
+			i -= 1;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
+	}
+	ptr[i] = NULL;
+	return (ptr);
+}                   
