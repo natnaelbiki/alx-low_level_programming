f134ba7e7f03a4169967feb51d9789b5fdb81fb0
natnaelbiki <natnaelbiki@gmail.com>
2023-02-06 10:41:17 -0800

    io files project

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -78,7 +78,7 @@ void print_class(unsigned char *e_ident)
 void print_data(unsigned char *e_ident)
 {
 	printf("  Data:                              ");
-	
+
 	switch (e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
@@ -116,7 +116,7 @@ void print_version(unsigned char *e_ident)
 void print_osabi(unsigned char *e_ident)
 {
 	printf("  OS/ABI:                            ");
-	
+
 	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_SYSV:
